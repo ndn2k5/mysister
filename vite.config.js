439837aded5d0a1a932b1b52f2681d855484aa2f
Nguyễn Duy Ngọc <102421837+ndn2k5@users.noreply.github.com>
2026-05-08 21:33:25 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Dùng './' để deploy được cả trên GitHub Pages dạng /ten-repo/
-  base: './',
+ base: '/mysister/',
 });
